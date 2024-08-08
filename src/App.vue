2644e77a9cfb39d09668d7e3acd5a5802356a3e6
Adam Stępień <vesemir2k@gmail.com>
2024-08-08 23:51:42 +0200

    🔧 (package.json): add sass dependency to support SCSS in the project 📝 (App.vue): update template content to include text for testing purposes 📝 (App.vue): remove scoped attribute from style tag to apply styles globally 📝 (main.js): update import statement to use style.scss file instead of main.css ✨ (style.scss): add new SCSS file to the project for styling purposes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div></div>
+  <div>dasda</div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './main.css'
+import './style.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
