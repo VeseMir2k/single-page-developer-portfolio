3997cf8a7a3a6a2541aea90afdc58077b84951e1
Adam Stępień <vesemir2k@gmail.com>
2024-08-11 21:02:13 +0200

    ✨ (BasicButton.vue): Create a new BasicButton component to display a styled button with link and text properties 📝 (HeaderIntro.vue): Refactor HeaderIntro component to use the newly created BasicButton component for the "CONTACT ME" button and improve styling for the intro section

---
+++ src/components/BasicButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <a class="button" :href="link">{{ text }}</a>
+</template>
+
+<script>
+export default {
+  name: 'BasicButton',
+  props: {
+    link: {
+      type: String
+    },
+    text: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  @include button-font;
+  text-transform: uppercase;
+  color: $white-color;
+  text-decoration: none;
+  padding-bottom: 12px;
+  position: relative;
+  &::after {
+    content: '';
+    display: block;
+    height: 2px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: $green-color;
+  }
+  &:hover {
+    color: $green-color;
+  }
+}
+</style>
--- src/components/header/HeaderIntro.vue
+++ src/components/header/HeaderIntro.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="intro">
-    <h1 class="intro__title">Nice to meet you! I’m Adam Keyes.</h1>
+    <h1 class="intro__title">Nice to meet you! I’m <span class="intro__name">Adam Keyes</span>.</h1>
     <p class="intro__description">
       Based in the UK, I’m a front-end developer passionate about building accessible web apps that
       users love.
     </p>
-    <button class="intro__button">CONTACT ME</button>
+    <BasicButton link="#" text="contact me" />
   </div>
 </template>
 
 <script>
-export default {}
+import BasicButton from '../BasicButton.vue'
+
+export default {
+  name: 'HeaderIntro',
+  components: { BasicButton }
+}
 </script>
 
 <style lang="scss" scoped>
 .intro {
+  margin: 40px 0 24px;
   text-align: center;
   &__title {
     @include heading-xl-mobile-font;
+    margin: 0 0 20px;
+  }
+  &__name {
+    display: inline-block;
+    border-bottom: 4px solid $green-color;
   }
   &__description {
     @include body-mobile-font;
