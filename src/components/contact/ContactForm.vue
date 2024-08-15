3200c75bed5f046fbfcc92d7c21b30eae56fb602
Adam Stępień <vesemir2k@gmail.com>
2024-08-15 19:42:38 +0200

    📝 (ContactForm.vue): Increase the number of rows in the message textarea to 4 for better user experience ♻️ (ContactForm.vue): Refactor styling in ContactForm.vue to improve readability and maintainability by removing unnecessary padding properties and adding z-index to the form header

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -2,7 +2,7 @@
   <form class="form" @submit.prevent="handleSubmitForm">
     <input class="form__input" type="text" placeholder="name" />
     <input class="form__input" type="text" placeholder="email" />
-    <textarea class="form__textarea" rows="1" placeholder="message"></textarea>
+    <textarea class="form__textarea" rows="4" placeholder="message"></textarea>
     <button class="form__button" type="submit">SEND MESSAGE</button>
   </form>
 </template>
@@ -29,7 +29,7 @@ export default {
   gap: 32px;
   position: relative;
 
-  &::before {
+  &::after {
     background-image: url('../../assets/pattern-rings.svg');
     bottom: 7px;
     content: '';
@@ -47,6 +47,8 @@ export default {
     border-bottom: 1px solid $white-color;
     color: $white-color;
     padding-left: 24px;
+    padding-bottom: 16px;
+    z-index: 1;
 
     &:focus {
       border-bottom-color: $green-color;
@@ -55,12 +57,6 @@ export default {
     &::placeholder {
       text-transform: uppercase;
     }
-  }
-  &__input {
-    padding-bottom: 16px;
-  }
-  &__textarea {
-    padding-bottom: 80px;
   }
   &__button {
     @include button-font;
